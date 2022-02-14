c7ece836910917a9912f7c7a9af2683e3a0b1660
harsimran1907 <97696473+harsimran1907@users.noreply.github.com>
2022-02-14 12:42:49 +0530

    Add files via upload

---
+++ assgn3_output.py
@@ -0,0 +1,122 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+Question 1
+Please give an input- harry
+Total occurances
+h-1
+a-1
+r-2
+y-1
+
+
+Question 1
+Please give an input- harry johal johal harry johal
+Total occurances
+harry-2
+johal-3
+
+Question 2
+Please enter Day- 18
+Please enter Month- 07
+Please enter Year- 2003
+Next Date is: 19/07/2003
+
+
+Question 3
+Plz enter Y to enter data- Y
+Plz give an integer input- 2
+Plz enter Y to enter data- Y
+Plz give an integer input- 5
+Plz enter Y to enter data- Y
+Plz give an integer input- 9
+Plz enter Y to enter data- Y
+Plz give an integer input- 37
+Plz enter Y to enter data- N
+[2, 5, 9, 37]
+[(2, 4), (5, 25), (9, 81), (37, 1369)]
+
+Question 4
+Enter Grade Points- 8
+Your Grade is B+ and Very Good performance 
+
+
+Question 5
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A 
+ 
+
+Question 6
+Please Enter SID of Student- 21105002
+Please enter the name of the Student- armeen singh
+If you want to enter more details press Y/N- Y
+Please Enter SID of Student- 21105003
+Please enter the name of the Student- harman singh
+If you want to enter more details press Y/N- Y
+Please Enter SID of Student- 21105004
+Please enter the name of the Student- pankaj kumar
+If you want to enter more details press Y/N- Y
+Please Enter SID of Student- 21105005
+Please enter the name of the Student- soumil arora
+If you want to enter more details press Y/N- Y
+Please Enter SID of Student- 21105006
+Please enter the name of the Student- harsimran
+If you want to enter more details press Y/N- N
+Part a
+21105002 is armeen singh
+21105003 is harman singh
+21105004 is pankaj kumar
+21105005 is soumil arora
+21105006 is harsimran 
+
+Part b
+value sorted dictionary is ['armeen singh', 'harman singh', 'Pankaj kumar', 'soumil arora', 'harsimran']
+
+Part c
+Keys sorted dictionary is [21105002, 21105003, 21105004, 21105005, 21105006]
+
+Part D
+Please enter the student's SID whose detail you need- 21105006
+Name of the required student is harsimran
+
+
+Question 7
+Total elements of Fibonacci sequence that you want(must be greater than 1)- 2
+Fibonnaci sequence with 2 terms
+0
+1
+Average of total 2 terms of sequence is 0.5
+END
+
+
+Question 7
+Total elements of Fibonacci sequence that you want(must be greater than 1)- 10
+Fibonnaci sequence with 10 terms
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+Average of total 10 terms of sequence is 8.8
+END
+
+
+Question 8
+Part a
+set with elements not common in both is {1, 3, 5, 6, 8}
+Part b
+Set of elements that is only present in one set is {17, 3, 6, 8, 9, 13}
+Part C
+Set of elements that is only present in two set is {1, 2, 4, 5}
+Part d
+set of all integers in the range 1 to 10 that are not in Set1 {6, 7, 8, 9, 10}
+Part e
+set of all integers in the range 1 to 10 that are not in Set1 and Set2 and Set3 {10, 7}
